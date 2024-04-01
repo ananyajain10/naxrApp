ac3b3ddbf917a91486f5b3e8f1ff114c8c62e99b
ananyajain10 <jainananya355@gmail.com>
2024-04-01 16:56:00 +0530

    logo

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,20 +3,21 @@ import Link from "next/link"
 import React from "react";
 import { cn } from "@/components/utils/cn";
 import { Button } from "@/components/ui/moving-border";
-import { Spotlight } from "@/components/ui/Spotlight";
+import { Meteors } from "@/components/ui/meteors";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
+const words = `Our mission is to fuel business growth through technology.` ;
+
 function HeroSection() {
   return (
     <div
     className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
     >
-          <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20 sm:left-40 sm:-top-10 "
-        fill="white"
-      />
+         
         <div className="p-4 relative z-10 w-full text-center">
        
 
-
+        <Meteors number={20} />
       
             <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold
@@ -27,9 +28,7 @@ function HeroSection() {
             <p
             className="mt-4 font-normal text-base md:text-lg text-neutral-300
             max-w-lg mx-auto"
-            >our mission is to fuel business growth through technology.
-                 We believe in harnessing the power of artificial intelligence to drive innovation, 
-                streamline operations, and enhance customer experiences.
+            > <TextGenerateEffect words={words} />
                 <div className="mt-4">
                     <Link href={"/product"}>
                     <Button
@@ -38,6 +37,7 @@ function HeroSection() {
       >
         Explore Product
       </Button>
+      <Meteors number={20} />
                     </Link>
                 </div>
             </p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import Logo from "./Logo";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "./utils/cn";
 import Link from "next/link";
@@ -8,8 +9,11 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-10 inset-x-0 max-w-5xl mx-auto z-50 flex flex-row justify-between", className)}
     >
+<div>
+<Logo/>
+</div>
       <Menu setActive={setActive}>
         <Link href={"/"}>
           <MenuItem
@@ -18,6 +22,7 @@ function Navbar({ className }: { className?: string }) {
             item="Home"
           ></MenuItem>
         </Link>
+        <Link href={"/services"}>
           <MenuItem setActive={setActive} active={active} item="Services">
             <div className="flex flex-col space-y-4 text-sm">
               <HoveredLink href="/web-dev">Website Development</HoveredLink>
@@ -29,7 +34,7 @@ function Navbar({ className }: { className?: string }) {
             </div>
           </MenuItem>
 
-
+          </Link>
           {/* <Link href={"/contact"}>
             <MenuItem
             setActive={setActive}
