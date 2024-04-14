e097be57c3b060aea415ddf40bd4e55ba83b6825
Daniel Ochuba <danielochuba78@gmail.com>
2024-04-14 16:40:09 +0100

    add feedback reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './actions/authSlice';
 import serviceReducer from './actions/serviceSlice';
-import applicationReducer from './actions/applicationSlice'
+import applicationReducer from './actions/applicationSlice';
+import feedbackReducer from './actions/feedbackSlice';
 
 
 export default configureStore({
     reducer: {
         auth: authReducer,
         service: serviceReducer,
-        application: applicationReducer
+        application: applicationReducer,
+        feedback: feedbackReducer
     },
 });
